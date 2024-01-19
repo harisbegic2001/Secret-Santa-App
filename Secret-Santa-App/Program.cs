f4ba11112cb61c7716009d27bebca5d6ce7a7a78
harisbegic2001 <harisbegic01@hotmail.com>
2024-01-19 18:38:38 +0100

    pushane promjene

--- Secret-Santa-App/Program.cs
+++ Secret-Santa-App/Program.cs
@@ -74,7 +74,7 @@ if (app.Environment.IsDevelopment())
 //Custom exception Middleware (GLOBAL ERROR HANDLING)
 app.ConfigureExceptionMiddleware();
 app.UseHttpsRedirection();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.UseAuthentication(); 
 app.UseAuthorization();
 
